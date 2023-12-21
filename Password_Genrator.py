ff2501866de89308efcdbfe64e4dde959f1a594f
Alok Ranjan <117595460+Aaki2003@users.noreply.github.com>
2023-12-21 16:23:44 +0530

    Create Password_Genrator.py

---
+++ Password_Genrator.py
@@ -0,0 +1,33 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+print("Easy Password")
+epwd = ""
+for i in range(0,nr_letters):
+  epwd = epwd+ random.choice(letters)
+for i in range(0,nr_symbols):
+  epwd+=random.choice(symbols)
+for i in range(0,nr_numbers):
+  epwd+=random.choice(numbers)
+print(epwd)
+
+hpwd = ""
+for i in range(0,len(epwd)):
+  hpwd+=random.choice(epwd)
+print("Hard password")
+print(hpwd)
